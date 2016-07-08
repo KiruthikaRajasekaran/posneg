f191bfe44bcf3654e9fefe9aa99b3656117b74f2
KiruthikaRajasekaran <krithikasadhana213@gmail.com>
2016-07-08 10:41:01 +0530

    finish

---
+++ README.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+int a,b;
+clrscr();
+printf("enter the value of a and b");
+scanf("%d%d",&a,&b);
+if(a>b)
+{
+printf("the value of a is positive");
+)
+else
+{
+printf("the value of b is positive");
+}
+getch();
+}
